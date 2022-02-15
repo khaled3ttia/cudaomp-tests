4bc86604392105779124504666691c28d78563f5
khaled3ttia <khaled.3ttia@gmail.com>
2022-02-14 22:24:50 -0600

    add error checking for device-side operations

--- atomicAdd/main.cu
+++ atomicAdd/main.cu
@@ -1,53 +1,56 @@
-#include <iostream>
 #include <cuda.h>
+#include <iostream>
 
-__global__ void testAdd(int *x){
+__global__ void testAdd(int *x) {
 
+  for (int i = 0; i < 10; i++) {
 
-    for (int i = 0 ; i < 10 ; i++){
-
-        atomicAdd(&x[i], 5);
-
-
-    }
-
+    atomicAdd(&x[i], 5);
+  }
 }
 
-int main(void){
+int main(void) {
 
-    int N = 10;
-    int *x, *d_x, *new_x;
+  int N = 10;
+  int *x, *d_x, *new_x;
 
-   
-    x = (int*)malloc(N*sizeof(int));
+  x = (int *)malloc(N * sizeof(int));
 
-    new_x = (int*)malloc(N*sizeof(int));
+  new_x = (int *)malloc(N * sizeof(int));
 
+  if (cudaMalloc(&d_x, N * sizeof(int)) != cudaSuccess) {
+    printf("Error: failed to  allocate %d bytes on device for d_x\n",
+           N * sizeof(int));
+  }
 
-    cudaMalloc(&d_x, N*sizeof(int));
+  for (int i = 0; i < N; i++) {
 
-    for (int i = 0 ; i < N; i++){
+    x[i] = 0.0;
+  }
 
-        x[i] = 0.0;
-    }
+  if (cudaMemcpy(d_x, x, N * sizeof(int), cudaMemcpyHostToDevice) !=
+      cudaSuccess) {
+    printf("Error: failed to copy %d bytes from host to device\n",
+           N * sizeof(int));
+  }
 
-    cudaMemcpy(d_x, x, N*sizeof(int), cudaMemcpyHostToDevice);
+  testAdd<<<1, 10>>>(d_x);
 
-    testAdd<<<1, 10>>>(d_x);
+  if (cudaMemcpy(new_x, d_x, N * sizeof(int), cudaMemcpyDeviceToHost) !=
+      cudaSuccess) {
+    printf("Error: failed to copy %d bytes from device to host\n",
+           N * sizeof(int));
+  }
 
-    
-    cudaMemcpy(new_x, d_x, N*sizeof(int), cudaMemcpyDeviceToHost);
+  for (int i = 0; i < N; i++) {
 
-    
-    for (int i = 0; i < N; i++){
+    printf("%d ", new_x[i]);
+  }
 
-        printf("%d ", new_x[i]); 
-    }
-    
+  if (cudaFree(d_x) != cudaSuccess) {
+    printf("Error: failed to free memory from the device\n");
+  }
 
-    cudaFree(d_x);
-    free(new_x);
-    free(x);
-    
-
+  free(new_x);
+  free(x);
 }
